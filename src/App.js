cf031731226ea22ed4ff2cc7321b0894ad1c9e44
Silas <silasjafarzadeh1029@gmail.com>
2023-08-13 20:52:19 -0400

    This is my first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://media.npr.org/assets/img/2017/09/12/macaca_nigra_self-portrait-3e0070aa19a7fe36e802253048411a38f14a79f8-s1100-c50.jpg" className="App-logo" alt="logo" />
+        <p>
+          monke
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
