5826a60f8ca6bd9221b15cc44aaca0180af2f066
Silas <silasjafarzadeh1029@gmail.com>
2023-08-16 21:25:44 -0400

    i change monke to monkey

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
       <header className="App-header">
         <img src="https://media.npr.org/assets/img/2017/09/12/macaca_nigra_self-portrait-3e0070aa19a7fe36e802253048411a38f14a79f8-s1100-c50.jpg" className="App-logo" alt="logo" />
         <p>
-          monke
+          monkey
         </p>
         <button
           className="App-link"
